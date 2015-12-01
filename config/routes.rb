85cd0621ee684485481721758343ab6107058006
Mariko <maripiyok@gmail.com>
2015-12-02 08:02:52 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  resources :drafts
+
+  resources :users, only: [ :show ], path: '', controller: :profile do
+    resources :items, only: [ :index ] do
+      resource :stock, only: [ :create, :update, :destroy ]
+      resource :comment, only: [ :create, :update, :destroy ]
+    end
+    resources :stocks, only: [ :index ]
+    resources :comments, only: [ :index ]
+  end
 end
